ca2526f72b459e5397ddd6cdd2d6e017dd1f3002
partridge5 <pte101@hotmail.com>
2016-11-18 23:59:06 -0500

    Add files via upload
    Second Commit

---
+++ js/yoga.js
@@ -0,0 +1,9 @@
+
+/*tubular plugin for video background*/
+/* global $ */
+$(document).ready(function() {
+   $('#wrapper').tubular({videoId: 'ahlywIKP5_c'});
+});
+
+$('#offer').delay(3000).fadeTo('medium', 0);
+//fadeOut('slow');
